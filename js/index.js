96098c587910a5872fbf04cf062b00aefebc8c81
Chukuwinke <rittnerodigwe@gmail.com>
2021-09-19 08:11:58 +0300

    added footer and modified pictures.json

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 const tabs = document.querySelector(".tabs");
+const tabItems = document.querySelectorAll(".tabs__title")
 const workButton = document.getElementById('work__btn')
 const galleryButton = document.getElementById('gallery__btn')
 const workImgContainer = document.querySelector(".work-tabs-img__container");
+let newsContainer = document.querySelector('.breaking-news')
 let linkName;
 
 $(".services-tabs-content li").first().css("display", "flex");
+$(".services-tabs__title").first().addClass("tab-active");
 $(".services-tabs__title").click(function () {
   $(".services-tabs__title.tab-active").removeClass("tab-active");
   $(this).addClass("tab-active");
@@ -17,6 +20,8 @@ fetch("pictures.json")
   .then((response) => response.json())
   .then((data) => {
     let links = Object.values(data.images);
+    let blogs = Object.values(data.articles)
+    //console.log(blogs)
     //function to populate work image container with images
     const imageBox = (links, imgItems) => {
       if (document.contains(imgItems)) {
@@ -32,6 +37,8 @@ fetch("pictures.json")
     const tabSwitcher = (links, target) => {
       let imgItems = document.querySelector(".work__img");
       let linkTab = target.getAttribute("data-work-tab");
+
+      target.style.border ='2px solid #18CFAB '
 
       if (linkTab == "all") {
         let linkArr = [];
@@ -54,8 +61,13 @@ fetch("pictures.json")
 
     //WORK SECTION
     // clicking a button targets one of the tabs
+    tabItems[0].style.border ='2px solid #18CFAB '
     tabs.onclick = (event) => {
       let target = event.target;
+      tabItems.forEach(element => {
+        element.style.border =''
+        
+      });
       if (target.className != "tabs__title") return;
       tabSwitcher(links, target);
     };
@@ -94,26 +106,42 @@ fetch("pictures.json")
       }
       
     }
-    //GALLERY SECTION
-    galleryButton.onclick = (event) => {
-      let galleryItems = 8;
-      const galleryContainer = document.querySelector('.gallery__img-container')
-      const elementList = [...document.querySelectorAll('.gallery .gallery__item')]
-      for (let i = galleryItems; i< galleryItems + 4; i++){
-        if (elementList[i]){
-          galleryContainer.style.height = '1500px'
-          elementList[i].style.display ='initial'
-        }
-      }
-      galleryItems += 4;
-
-      if (galleryItems >= elementList.length){
-        event.target.style.display = 'none'
-      }
-    }
+    
+    // breaking news
+    //console.log(blogs.author)
+    blogs.map(blog => {
+      const newsWrapper = document.createElement('div')
+      newsWrapper.className = 'breaking-news__card'
+      newsWrapper.innerHTML =`
+        <img src="${blog.url}" alt="" class="breaking-news__img">
+        <div class="breaking-news__info">
+          <h3 class="breaking-news__title">${blog.title}</h3>
+          <p class="breaking-news__text">${blog.author}<span class="breaking-news__author"></span>${blog.commentsNo} comment</p>
+        </div>
+        `
+      newsContainer.appendChild(newsWrapper)
+    })
 
   })
   .catch((error) => console.log(error));
+
+  //GALLERY SECTION
+  galleryButton.onclick = (event) => {
+    let galleryItems = 8;
+    const galleryContainer = document.querySelector('.gallery__img-container')
+    const elementList = [...document.querySelectorAll('.gallery .gallery__item')]
+    for (let i = galleryItems; i< galleryItems + 4; i++){
+      if (elementList[i]){
+        galleryContainer.style.height = '1500px'
+        elementList[i].style.display ='initial'
+      }
+    }
+    galleryItems += 4;
+
+    if (galleryItems >= elementList.length){
+      event.target.style.display = 'none'
+    }
+  }
 
   // feedback logic
 let sliderThumbnails = document.querySelectorAll('.slider-thumbnail');
